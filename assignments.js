4f4ce84ba79b8d4bf51ed916a5564444b32f5e88
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2022-06-18 13:16:11 +0300

    variables

---
+++ assignments.js
@@ -0,0 +1,6 @@
+let country = "Ukraine";
+let continent = "Europe";
+let population = "40 millions";
+console.log(country);
+console.log(continent);
+console.log(population);
--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JS is FUN!');
 console.log(40+8+23-10);
+console.log("Jonas");
+console.log(23);
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+let PI = 3.1415;
+
